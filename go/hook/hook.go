07960398278bf3419af5d699e07cefcc0beb1f0b
xiaodong <xiaodong@xiaodongdeMBP.lan>
2023-08-12 10:13:28 +0800

    add hook

---
+++ go/hook/hook.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	hook := MyHook{}
+	DoSome(&hook)
+
+}
+
+func DoSome(hook Hook) {
+	hook.Before()
+
+	hook.After()
+}
+
+type Hook interface {
+	// 钩子方法
+	Before()
+	After()
+}
+
+type MyHook struct {
+	Hook Hook
+}
+
+func (m *MyHook) Before() {
+	fmt.Println("before this is a hook")
+}
+
+func (m *MyHook) After() {
+	fmt.Println("after this is a hook")
+}
